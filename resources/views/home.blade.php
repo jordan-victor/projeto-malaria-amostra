365d8a0e7029bd7c0a808d1804e0b8856bed1d4b
Jordan Victor <jordanvictor63@gmail.com>
2024-08-09 16:31:56 -0400

    front da home quase finalizada

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,20 +4,41 @@
 
 <main id="home">
     <header class="container">
-        @include('layouts.navbar')
         <h1 style="line-height:70px">MONITORAMENTO DIGITAL</h1>
-        <div class="imgHome">
-            <img src="/img/home/familia.png" alt="Ilustração de uma família">
-        </div>
+        <section class="cards1">
+            <div class="carta">
+                <img src="/img/home/localidades.png" alt="Malariômetro Localidades">
+            </div>
+
+            <div class="imgHome">
+                <img src="/img/home/familia.png" alt="Ilustração de uma família">
+            </div>
+
+            <div class="carta">
+                <img src="/img/home/notificantes.png" alt="Unidades Notificantes">
+            </div>
+        </section>
+
+
+
+        <section class="cards2">
+            <div class="carta">
+                <img src="/img/home/alerta.png" alt="Alerta Malária" style="width:250px">
+            </div>
+
+            <div class="carta">
+                <img src="/img/home/indicadores.png" alt="Malariômetro Localidades">
+            </div>
+        </section>
+
+
+
+        <section class="cards3">
+            <div class="carta">
+                <img src="/img/home/criadouros.png" alt="Criadouros">
+            </div>
+        </section>
     </header>
+    
 </main>
-
-
-
-
-<section>
-    <form action="" method="post">
-        
-    </form>
-</section>
 @endsection
--- resources/views/layouts/layoutPadrao.blade.php
+++ resources/views/layouts/layoutPadrao.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Malária Manaus</title>
+    <title>Malária Manaus @yield('title')</title>
     <!--BOOTSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!--FONTES E ÍCONES-->
@@ -16,32 +16,45 @@
     <link rel="stylesheet" href="/css/home.css">
 </head>
 
-<body>
-    <header class="container">
-        
-    </header>
+<body id="body">
+    <!--TODO O CONTEÚDO DAS OUTRAS PÁGINAS SERÃO IMPORTADOS PARA DENTRO DESSE CONTAINER-->
+    <div class="container">
+       <header>
+            @include('layouts.navbar')
+       </header>
+
+        @yield('content')   
+    </div>
 
 
 
 
 
-   @yield('content')
+   
    
 
 
 
 
    <footer>
-   </footer>
-   
-   
+        <div class="container">
+            <img src="/img/geral/prefeitura.png" width="250px" alt="">
+            
+            <div>
+                <p>&copy;{{$data}} - Prefeitura de Manaus</p>
+                <p>Secretaria Municipal de Saúde - SEMSA</p>
+                <p>Departamento do Distrito de Saúde Rural - DISAR</p>
+                <p>Gerência de Inteligência de Dados Rural - GEIND-R</p> 
+            </div>        
+        </div>
+   </footer>  
 </body>
-    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.bundle.min.js"></script>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="/b_js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.bundle.min.js"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+<script src="/b_js/bootstrap.min.js"></script>
 </html>
